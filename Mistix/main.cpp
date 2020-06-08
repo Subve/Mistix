87709598480a0b67a2018e5f281b1d3d6084e2ad
Subve <60174799+Subve@users.noreply.github.com>
2020-06-08 02:34:46 +0200

    Update main.cpp

--- Mistix/main.cpp
+++ Mistix/main.cpp
@@ -3,9 +3,22 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(512, 512), "Mistix", sf::Style::Close | sf::Style::Titlebar);
+	sf::RenderWindow window(sf::VideoMode(512, 512), "Mistix", sf::Style::Close | sf::Style::Resize);
 	while (window.isOpen())
 	{
+		sf::Event e;
+		while (window.pollEvent(e))
+		{	switch(e.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::Resized:
+				std::cout <<"New window width "<< e.size.width <<" New window height "<<e.size.height << std::endl;
+				break;
+			}
+			
+		}
 
 	}
 
