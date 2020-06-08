6c3a400e2339174610772461919657492dbacaa5
Subve <60174799+Subve@users.noreply.github.com>
2020-06-08 13:21:37 +0200

    A Simple Platform

--- Mistix/main.cpp
+++ Mistix/main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "Animation.h"
+#include "Player.h"
+#include "Platform.h"
+
+static const float VIEW_HEIGHT = 512.0f;
+
+void ResizeView(const sf::RenderWindow& window, sf::View& view)
+{
+	float aspectRatio = float(window.getSize().x) / float(window.getSize().y);
+	view.setSize(VIEW_HEIGHT * aspectRatio, VIEW_HEIGHT);
+}
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(512, 512), "Mistix", sf::Style::Close | sf::Style::Resize);
 	
-	sf::RectangleShape player(sf::Vector2f(100.0f, 150.0f));
-	player.setPosition(206.0f, 206.0f);
+	sf::View view(sf::Vector2f(0.0f,0.0f),sf::Vector2f(VIEW_HEIGHT,VIEW_HEIGHT));
+	
 	sf::Texture playerTexture;
 	playerTexture.loadFromFile("tux_from_linux.png");
-	player.setTexture(&playerTexture);
+	
 
-	Animation animation(&playerTexture,sf::Vector2u(3,9),0.3f);
+	Player player(&playerTexture,sf::Vector2u(3,9),0.3f,100.0f);
 
+	Platform platform1(nullptr, sf::Vector2f(400.0f, 200.0f), sf::Vector2f(500.0f, 200.0f));
+	Platform platform2(nullptr, sf::Vector2f(400.0f, 200.0f), sf::Vector2f(500.0f, 0.0f));
 	float deltaTime = 0.0f;
 	sf::Clock clock;
 
@@ -25,21 +37,34 @@ int main()
 		while (window.pollEvent(e))
 		{	switch(e.type)
 			{
-			case sf::Event::Closed:
-				window.close();
-				break;
+				case sf::Event::Closed:
+					window.close();
+					break;
+
+				case sf::Event::Resized:
+					ResizeView(window, view);
+					break;
 			
 				
 			}
 			
 		}
 		
-		animation.Update(0, deltaTime,false);
-		player.setTextureRect(animation.uvRect);
+		player.Update(deltaTime);
+		Collider col = player.GetCollider();
+		platform1.GetCollider().CheckCollision(col, 0.0f);
+		platform2.GetCollider().CheckCollision(col, 1.0f);
+
+		view.setCenter(player.GetPosition());
+		
 
 		window.clear(sf::Color(150,150,150));
 
-		window.draw(player);
+		window.setView(view);
+		
+		player.Draw(window);
+		platform1.Draw(window);
+		platform2.Draw(window);
 		window.display();
 
 	}
