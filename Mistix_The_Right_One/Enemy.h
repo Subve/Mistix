a4a625ee83e65da246767438f09bcf5c9db94a66
Subve <60174799+Subve@users.noreply.github.com>
2020-07-07 02:45:28 +0200

    Update Enemy
    breaking down the class into additional dedicated ABSTRACT class files

---
+++ Mistix_The_Right_One/Enemy.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include "Menu.h"
+#include <cmath>
+#include <math.h>
+#include <memory>
+#include <sstream>
+#include "Score.h"
+#include "Player.h"
+#include "Bullet.h"
+class Enemy :public sf::Sprite
+{
+
+private:
+
+public:
+
+	Enemy() :Sprite() {}
+		;
+	virtual ~Enemy() = default;
+	virtual void Follow() = 0;
+	virtual void Attack(Player& gracz) = 0;
+	virtual void Rotate(sf::Vector2f& playermove) = 0;
+	virtual void adMove(sf::Vector2f& vektorRuchu) = 0;
+	virtual void setDirection(sf::Vector2u& direction2) = 0;
+	virtual void setMove(sf::Vector2f& playermove) = 0;
+	virtual void killedZombie(Score& wynik, std::vector<Bullet>& pociski, std::vector<std::unique_ptr<Enemy>>& enemies) = 0;
+
+	float angle;
+	sf::Vector2u direction;
+	sf::Vector2f playerCenter;
+	sf::Vector2f mousePosWindow;
+	sf::Vector2f aimDir;
+	sf::Vector2f aimDirNorm;
+	sf::Vector2f enemyPos;
+	bool trafiony;
+	bool zabity;
+	int HP;
+
+};
+
